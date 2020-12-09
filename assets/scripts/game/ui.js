ff1aafca37954fd7be190870de6d1db52a7ebb47
hankd11234 <hankd11234@gmail.com>
2020-12-09 14:53:14 -0500

    Cleaned up

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -3,7 +3,6 @@ const store = require('./../store')
 const gameStartSuccess = function(response) {
   $('#message').text(null)
   store.game = response.game
-  console.log(store);
 }
 
 const onError = function (error) {
